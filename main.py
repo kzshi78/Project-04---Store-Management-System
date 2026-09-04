b669ce5d7dc37078e0d2507cc53fd715fd86712e
Kazem Sh <kzshi78@gmail.com>
2026-09-04 11:42:58 +0330

    Implement Order workflow and order item calculations

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from models import Product, Customer
+from models import Product, Customer, OrderItem, Order
 
-product = Product(1, "Mouse", 10, 500000, "2027-01-01")
+product_1 = Product(1, "Mouse", 10, 500000, "2027-01-01")
+product_2 = Product(2, "kyboard", 30, 10000, "2028-01-01")
 customer = Customer(1, "Ali", 2000000, "09123456789")
+item_1 = OrderItem(product_1, 3)
+item_2 = OrderItem(product_2, 5)
+order = Order(123, customer , "9/4/20206")
+order.add_item(item_1)
+order.add_item(item_2)
+print(order)
 
-print(product)
-print("----------------")
-print(customer)
 
 
 
--- models.py
+++ models.py
@@ -47,11 +47,62 @@ class OrderItem:
     
     def __str__(self):
         return (
-            f"Product: {self.product}"
-            f"Quantity: {self.quantity}"
-            f"Subtotal: {self.calculate_subtotal}"
+            f"Product: {self.product.name}\n"
+            f"Quantity: {self.quantity}\n"
+            f"Subtotal: {self.calculate_subtotal()}"
         )
 
 class Order():
-    def __init__(self):
-        pass
+    def __init__(self, order_id, customer, date):
+        self.order_id = order_id
+        self.customer = customer
+        self.items = []
+        self.date = date
+        self.status = "Pending"
+        
+    def add_item(self, new_item):
+        self.items.append(new_item)
+
+    def remove_item(self, item):
+        for it in self.items:
+            if it == item:
+                self.items.remove(item)
+                return
+        print("Not Found Item")
+
+    def calculate_total(self):
+        total = 0
+        for item in self.items:
+            total += item.calculate_subtotal() 
+        return total
+
+    def show_order(self):    
+        order_info = (
+            f"Order_ID: {self.order_id}\n"
+            f"Customer: {self.customer}\n"
+            f"Date: {self.date}\n"
+            f"Items:\n"
+        )
+
+        for item in self.items:
+            order_info += str(item) + "\n"
+
+        order_info += (
+            f"Total_Price: {self.calculate_total()}\n"
+            f"Status: {self.status}"
+        )
+
+        return order_info
+    
+    def complete_order(self):
+        self.status = "Complete"
+        
+
+    def cancel_order(self):
+      if self.status == "Pending":
+        self.status = "Cancelled"
+        return True
+      
+      return False
+    def __str__(self):
+        return self.show_order() 
